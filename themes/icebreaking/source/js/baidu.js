26cefb8eb0eefda660ac1750cdddf7c80019cab6
董浩 <donghao@edgarhao.local>
2019-04-29 22:08:35 +0800

    ...

--- themes/icebreaking/source/js/baidu.js
+++ themes/icebreaking/source/js/baidu.js
@@ -1,7 +1,22 @@
+// 百度统计
 var _hmt = _hmt || [];
 (function() {
 var hm = document.createElement("script");
 hm.src = "https://hm.baidu.com/hm.js?1316d0e28593d0403cc5c34683bb79cb";
 var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(hm, s);
+})();
+
+// 百度推送
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
 })();
